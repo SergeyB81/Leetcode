119bc4ab76f2ae8e263234c04e3994ee94bc6377
Sergey Bykov <bykov_sy@mail.ru>
2025-03-26 18:04:50 +0300

    repeat

--- pythonProject_41/sorting.py
+++ pythonProject_41/sorting.py
@@ -72,6 +72,6 @@ if __name__ == '__main__':
 
    # nums = [4, 8, 2, 1, 3]
    # bubble_sort(nums)
-    print(insertion_sort(nums))
+    print(quick_sort_op(nums))
     print(nums)
-#selection_sort 21.03.2025
+#selection_sort 21.03.2025 ++
