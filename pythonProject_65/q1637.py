f26128410e025ad566a10696e3a649b5708b0940
Sergey Bykov <bykov_sy@mail.ru>
2025-05-06 15:33:45 +0300

    sart

---
+++ pythonProject_65/q1637.py
@@ -0,0 +1,26 @@
+
+from typing import List
+
+class Solution:
+    def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
+        points.sort(key=lambda x: x[0])
+        gaps = []
+        n = len(points)
+        for i in range(1,n):
+            gaps.append(points[i][0] - points[i-1][0])
+
+        return max(gaps)
+
+    def maxWidthOfVerticalArea1(self, points: List[List[int]]) -> int:
+        A = sorted(x for x,y in points)
+
+        return max( [b - a for a,b in zip(A, A[1:])] + [0])
+
+
+
+
+
+if __name__ == ('__main__'):
+    print('sergey')
+    points = [[8, 7], [9, 9], [7, 4], [9, 7]]
+    print(Solution().maxWidthOfVerticalArea1(points))
--- pythonProject_65/q1791.py
+++ pythonProject_65/q1791.py
@@ -1,2 +1,19 @@
+from typing import List
+
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
+        if edges[0][0] in edges [1]:
+            return edges[0][0]
+        else: return edges[0][1]
+
+    def findCenter1(self, edges: List[List[int]]) -> int:
+        return (set(edges[0]) & set(edges[1])).pop()
+
+
+
+
+
+if __name__ == '__main__':
+    print('sergey')
+    edges = [[1,2],[5,1],[1,3],[1,4]]
+    print(Solution().findCenter1(edges))
