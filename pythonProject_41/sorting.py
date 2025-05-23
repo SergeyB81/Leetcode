b398d509e3908dce76ca6df916f7c9887c1972c1
Sergey Bykov <bykov_sy@mail.ru>
2025-05-23 16:45:46 +0300

    rep

--- pythonProject_41/sorting.py
+++ pythonProject_41/sorting.py
@@ -72,6 +72,6 @@ if __name__ == '__main__':
    # nums = [4, 8, 2, 1, 3]
    #bubble_sort(nums)
    # print(partition(nums, 0 ,6))
-    insertion_sort(nums)
+    print(quick_sort_op(nums))
     print(nums)
 # rep new 4-1
