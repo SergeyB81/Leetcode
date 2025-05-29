4a38f8d9ff8fc6d06a5254680665950d6e89f759
Sergey Bykov <bykov_sy@mail.ru>
2025-05-29 12:02:12 +0300

    rep

---
+++ pythonProject_65/q1486.py
@@ -0,0 +1,16 @@
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        xor = start
+        for i in range(1,n):
+            xor =xor ^ (start + 2 * i)
+        return xor
+
+
+
+
+
+if __name__ == ('__main__'):
+    print('sergey')
+    n = 4
+    start = 3
+    print(Solution().xorOperation(n, start))
