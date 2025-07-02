671084800d10836ea570cfd0a6f5965c8886a596
Sergey Bykov <bykov_sy@mail.ru>
2025-07-02 16:39:12 +0300

    rep5

---
+++ pythonProject_71/q760.py
@@ -0,0 +1,36 @@
+from typing import List
+
+class Solution:
+    def anagramMappings(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        mapping = []
+
+        # O(n**2)
+        for el in nums1:
+            mapping.append(nums2.index(el))
+        return mapping
+
+
+class Solution1:
+    def anagramMappings(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        mapping_d = {}
+        # O(n)
+        for i, el in enumerate(nums2):
+            mapping_d[el] = i
+        #return mapping_d
+
+        # O(n)
+        mapping = []
+        for el in  nums1:
+            mapping.append(mapping_d[el])
+        return mapping
+
+
+
+
+if __name__ == '__main__':
+    print('sergey')
+    nums1 = [12, 28, 46, 32, 50]
+    nums2 = [50, 12, 32, 46, 28]
+    print(Solution1().anagramMappings(nums1,nums2))
+
+
