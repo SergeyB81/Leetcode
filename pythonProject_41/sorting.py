f5a176daf6eaa8e34ca5215a121b9578fdd4cee3
Sergey Bykov <bykov_sy@mail.ru>
2025-07-23 14:34:24 +0300

    rep

--- pythonProject_41/sorting.py
+++ pythonProject_41/sorting.py
@@ -69,6 +69,12 @@ def partition(arr, start, end):
     # Возвращаем индекс, на котором теперь находится опорный элемент
     return i + 1
 
+# Этот код реализует алгоритм быстрой сортировки (Quick Sort) «на месте» с помощью
+# функции partition, которая выбирает в качестве опорного (pivot) последний элемент
+# одмассива, а затем разделяет массив на элементы, меньшие и не меньшие опорного.
+# Quick Sort в среднем работает за O(n·log n), однако в худшем случае
+# (например, при уже отсортированном массиве и выборе крайних элементов в качестве pivot)
+# сложность может вырасти до O(n²).
 
 # Рекурсивная функция быстрой сортировки
 def quick_sort(arr, start, end):
@@ -90,12 +96,12 @@ if __name__ == '__main__':
     print('sergey')
     nums = [4,8,0,10,2,1,3,2,9,7]
    # nums = [2,2,2,2]
-    quick_sort(nums, 0, len(nums)-1)
+   # quick_sort(nums, 0, len(nums)-1)
    # insertion_sort(nums)
    # selection_sort(nums)
    # nums = [4, 8, 2, 1, 3]
    # bubble_sort(nums)
-   # print(partition(nums, 0 ,6))
+    print(partition(nums, 0,7))
    # print(quick_sort(nums,0,9))
     print(nums)
 # rep 5+
