04dcd0107790bed2925e6f53d635ceca7607c688
SergeyB81 <bykov_sy@mail.ru>
2025-09-08 15:41:06 +0300

    good

--- pythonProject_72/q121.py
+++ pythonProject_72/q121.py
@@ -16,31 +16,9 @@ class Solution1:
         # возвращаем максимальную прибыль или 0, если все отрицательные
         return max(max(deals), 0)
 
-# O(n**2)
+
+
 class Solution2:
-    def maxProfit(self, prices: List[int]) -> int:
-
-        while len(prices) > 1:
-            # Находим минимум и максимум
-            min_el = min(prices)
-            max_el = max(prices)
-
-            # Находим их индексы
-            min_el_ind = prices.index(min_el)
-            max_el_ind = prices.index(max_el)
-
-            # Если максимум после минимума — можно купить-продать
-            if max_el_ind > min_el_ind:
-                return max_el - min_el
-            else:
-                # Иначе удаляем максимум и пробуем снова
-                del prices[max_el_ind]
-
-        return 0  # если не нашли подходящую пару
-
-
-
-class Solution3:
     def maxProfit(self, prices: List[int]) -> int:
         if len(prices) < 2:
             return 0
@@ -65,7 +43,7 @@ class Solution3:
         return cur_max
 
 
-class Solution4:
+class Solution3:
     def maxProfit(self, prices):
         max_profit = 0
         min_price = float('inf')  # инициализируем бесконечным минимумом
@@ -83,7 +61,7 @@ class Solution4:
         return max_profit
 
 if __name__ == '__main__':
-    prices = [7, 3, 12 ,5, 1, 8, 11, 4]
-    print(Solution4().maxProfit(prices))
+    prices = [7, 3, 15 ,5, 1, 8, 11, 4]
+    print(Solution2().maxProfit(prices))
 
     # rep5
