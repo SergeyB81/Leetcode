85faa6516e83db3475773a9dcd62d479b2066f74
SergeyB81 <bykov_sy@mail.ru>
2025-09-27 17:23:03 +0300

    nice

--- pythonProject_35/q1480_cumsum.py
+++ pythonProject_35/q1480_cumsum.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     def runningSum(nums: List[int]) -> List[int]:
         for i in range(1, len(nums)):
             nums[i] = nums[i] + nums[i-1]
-        return num
+        return nums
 
 
     print(runningSum(nums))
