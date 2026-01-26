ed99c2afff0cdc50a35d464d509ecdaf82974b10
SergeyB81 <bykov_sy@mail.ru>
2026-01-26 20:56:20 +0300

    Thank you

---
+++ pythonProject_55/1.py
@@ -0,0 +1,5 @@
+for i in range(7):
+    if i == 4:
+        print('Привет Ваня!')
+    else:
+        print('Привет Оля!')
--- pythonProject_55/q1920.py
+++ pythonProject_55/q1920.py
@@ -19,4 +19,4 @@ if __name__ == "__main__":
     print('hello')
 
 
-    # rep5
+    # rep5+
