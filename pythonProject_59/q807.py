2199390a9121a84320c78d0b28ba0e2f956bb81e
SergeyB81 <bykov_sy@mail.ru>
2026-01-29 17:51:12 +0300

    nice

--- pythonProject_59/q807.py
+++ pythonProject_59/q807.py
@@ -24,4 +24,4 @@ class Solution:
 if __name__ == '__main__':
     grid = [[3, 0, 8, 4], [2, 4, 5, 7], [9, 2, 6, 3], [0, 3, 1, 0]]
     print(Solution().maxIncreaseKeepingSkyline(grid))
-#rep5
+#rep5+
