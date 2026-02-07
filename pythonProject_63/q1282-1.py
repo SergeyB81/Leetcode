25b00aa09fde7e1e457146b80ac0604d45d6f62d
SergeyB81 <bykov_sy@mail.ru>
2026-02-07 19:49:11 +0300

    try

--- pythonProject_63/q1282-1.py
+++ pythonProject_63/q1282-1.py
@@ -10,6 +10,8 @@ class Solution:
         for person_id, group_size in enumerate(groupSizes):
             size_to_people[group_size].append(person_id)
 
+        print(size_to_people)
+
         for group_size, people in size_to_people.items():
             for i in range(0, len(people), group_size):
                 result.append(people[i:i+group_size])
