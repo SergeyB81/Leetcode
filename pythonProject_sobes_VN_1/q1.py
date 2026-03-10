86557f194c5311947a85c4621e57b990d2c1798f
SergeyB81 <bykov_sy@mail.ru>
2026-03-10 12:16:44 +0300

    good

--- pythonProject_sobes_VN_1/q1.py
+++ pythonProject_sobes_VN_1/q1.py
@@ -14,3 +14,5 @@ print(unique_in_order('AAAABBBCCDAABBB'))  # ['A', 'B', 'C', 'D', 'A', 'B']
 print(unique_in_order('ABBCCAD'))         # ['A', 'B', 'C', 'A', 'D']
 print(unique_in_order([1, 2, 2, 3, 3]))   # [1, 2, 3]
 print(unique_in_order([]))
+
+# 1
